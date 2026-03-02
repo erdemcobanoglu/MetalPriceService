f4aba903f1211758faf2f900b97b06c3b892f707
erdem çobanoglu <erdemcobanoglu1122@hotmail.com>
2026-03-02 17:28:39 +0000

    tcmb worker folder structure

---
+++ TcmbRatesWorker/Application/Abstractions/IRatesRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcmbRatesWorker.Application.Abstractions
+{
+    public interface IRatesRepository
+    {
+    }
+}
---
+++ TcmbRatesWorker/Program.cs
@@ -0,0 +1,7 @@
+using TcmbRatesWorker.Worker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
